42ccd482c5226ac923231366cbae1eff6a2915a1
vaka21fc <vaka21fc@student.ju.se>
2023-10-05 12:46:39 +0200

    created a canvas with a random shape in a random color, which generates on mouse click

---
+++ finalProjectCode.js
@@ -0,0 +1,28 @@
+//Defining variables
+let shapes;
+let colors;
+
+//Setup Canvas
+function setup() {
+  createCanvas(300, 400);
+  noStroke();
+  shapes = ["triangle", "ellipse", "rectangle"];
+  colors = [color(255, 0, 0), color(0, 255, 0), color(0, 0, 255)];
+}
+
+function draw() {
+  background(232, 228, 214);
+  fill(colors[floor(random(0, 3))]);
+  let randomShape = shapes[floor(random(0, 3))];
+  if (randomShape == "ellipse") {
+    ellipse(random(100, 250), random(150, 250), random(20, 50), random(20, 50));
+  }
+  if (randomShape == "rectangle") {
+    rect(random(100, 250), random(150, 250), random(20, 50), random(20, 50));
+  }
+  noLoop();
+}
+
+function mousePressed() {
+  loop();
+}
