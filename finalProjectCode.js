59747f4c5f9520ce138bea7b167436b8a417d397
vaka21fc <vaka21fc@student.ju.se>
2023-10-06 13:04:24 +0200

    Half-ellipse fixing
    I did not want all of the half-ellipses to each be their own shape, as it would increase the chance that the system would pick more half-ellipses instead of the other shapes.
    
    My solution was to make one shape called "half-ellipse". Then I added if-statements with their own unique rotation of the half-ellipse. When the system chooses half-ellipse as the shape, the if-statement will decide the rotation.

--- finalProjectCode.js
+++ finalProjectCode.js
@@ -1,34 +1,93 @@
 //Defining variables
 let shapes;
 let colors;
+let spacingX;
+let spacingY;
 
 //Setup Canvas
 function setup() {
   createCanvas(353, 500);
   noStroke();
-  shapes = ["half-ellipse-left", "ellipse", "rectangle"];
+  shapes = ["half-ellipse", "ellipse", "rectangle"];
   colors = [color(255, 0, 0), color(0, 255, 0), color(0, 0, 255)];
   ellipseMode(CORNER);
+  spacingX = 45;
+  spacingY = 55;
 }
 
 function draw() {
   background(232, 228, 214);
   fill(255);
-  rect(30,30, 293, 390);
-  for (let i = 0; i < 4; i += 1) {
-    for (let l = 0; l < 7; l += 1) {
+  rect(30, 30, 293, 390);
+  for (let i = 0; i < 5; i += 1) {
+    for (let l = 0; l < 6; l += 1) {
       fill(colors[floor(random(0, 3))]);
       let randomShape = shapes[floor(random(0, 3))];
       if (randomShape == "ellipse") {
-        ellipse(70 + 60 * i, 40 + 50 * l, random(30, 40));
+        ellipse(50 + spacingX * i, 40 + spacingY * l, random(30, 40));
       }
       if (randomShape == "rectangle") {
-        rect(50 + 60 * i, 40 + 50 * l, random(30, 40), random(20, 40));
+        rect(
+          50 + spacingX * i,
+          40 + spacingY * l,
+          random(30, 40),
+          random(20, 40)
+        );
       }
-      if(randomShape =="half-ellipse-left"){
-        let startAngle = HALF_PI;
-        let endAngle = -HALF_PI;
-        arc(50+60*i, 40 + 50 * l, 30, 30, startAngle, endAngle, PIE);
+      if (randomShape == "half-ellipse") {
+        let randomHalfEllipse = floor(random(0, 4));
+        if (randomHalfEllipse == 0) {
+          let startAngle = HALF_PI;
+          let endAngle = -HALF_PI;
+          arc(
+            50 + spacingX * i,
+            40 + spacingY * l,
+            30,
+            30,
+            startAngle,
+            endAngle,
+            PIE
+          );
+        }
+        if (randomHalfEllipse == 1) {
+          let startAngle = PI;
+          let endAngle = 0;
+          arc(
+            50 + spacingX * i,
+            40 + spacingY * l,
+            30,
+            30,
+            startAngle,
+            endAngle,
+            PIE
+          );
+        }
+        if (randomHalfEllipse == 2) {
+          let startAngle = -HALF_PI;
+          let endAngle = HALF_PI;
+          arc(
+            50 + spacingX * i,
+            40 + spacingY * l,
+            30,
+            30,
+            startAngle,
+            endAngle,
+            PIE
+          );
+        }
+        if (randomHalfEllipse == 3) {
+          let startAngle = 0;
+          let endAngle = PI;
+          arc(
+            50 + spacingX * i,
+            40 + spacingY * l,
+            30,
+            30,
+            startAngle,
+            endAngle,
+            PIE
+          );
+        }
       }
     }
   }
