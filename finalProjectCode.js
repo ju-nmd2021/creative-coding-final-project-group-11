e95269c4790161c6200f5ce534366ab6b6fed09f
vaka21fc <vaka21fc@student.ju.se>
2023-10-06 12:25:17 +0200

    Changed canvas size to a4 proportions

--- finalProjectCode.js
+++ finalProjectCode.js
@@ -4,7 +4,7 @@ let colors;
 
 //Setup Canvas
 function setup() {
-  createCanvas(300, 400);
+  createCanvas(353, 500);
   noStroke();
   shapes = ["triangle", "ellipse", "rectangle"];
   colors = [color(255, 0, 0), color(0, 255, 0), color(0, 0, 255)];
@@ -12,19 +12,18 @@ function setup() {
 
 function draw() {
   background(232, 228, 214);
-  for (let i = 0; i < 5; i+=1) {
-    for(let l = 0; l < 7; l+=1){
-        fill(colors[floor(random(0, 3))]);
-        let randomShape = shapes[floor(random(0, 3))];
+  for (let i = 0; i < 4; i += 1) {
+    for (let l = 0; l < 7; l += 1) {
+      fill(colors[floor(random(0, 3))]);
+      let randomShape = shapes[floor(random(0, 3))];
 
-        if (randomShape == "ellipse") {
-          ellipse(30 + 60*i, 30+50*l, random(30, 40));
-        }
-        if (randomShape == "rectangle") {
-          rect(3+ 60*i, 30+50*l, random(30, 40), random(20, 40));
-        }
+      if (randomShape == "ellipse") {
+        ellipse(40 + 60 * i, 40 + 50 * l, random(30, 40));
+      }
+      if (randomShape == "rectangle") {
+        rect(40 + 60 * i, 40 + 50 * l, random(30, 40), random(20, 40));
+      }
     }
-    
   }
   noLoop();
 }
@@ -32,7 +31,6 @@ function draw() {
 function mousePressed() {
   loop();
 }
-
 
 /*
 
