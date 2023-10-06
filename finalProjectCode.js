cb613f1c175c8e026c48a3d4c1b960acd927bfd7
vaka21fc <vaka21fc@student.ju.se>
2023-10-06 12:38:53 +0200

    Added a rectangle as a guide for my "inner frame" on the canvas. Changed ellipseMode and added half-ellipses to my shape array

--- finalProjectCode.js
+++ finalProjectCode.js
@@ -6,25 +6,35 @@ let colors;
 function setup() {
   createCanvas(353, 500);
   noStroke();
-  shapes = ["triangle", "ellipse", "rectangle"];
+  shapes = ["half-ellipse-left", "ellipse", "rectangle"];
   colors = [color(255, 0, 0), color(0, 255, 0), color(0, 0, 255)];
+  ellipseMode(CORNER);
 }
 
 function draw() {
   background(232, 228, 214);
+  fill(255);
+  rect(30,30, 293, 390);
   for (let i = 0; i < 4; i += 1) {
     for (let l = 0; l < 7; l += 1) {
       fill(colors[floor(random(0, 3))]);
       let randomShape = shapes[floor(random(0, 3))];
-
       if (randomShape == "ellipse") {
-        ellipse(40 + 60 * i, 40 + 50 * l, random(30, 40));
+        ellipse(70 + 60 * i, 40 + 50 * l, random(30, 40));
       }
       if (randomShape == "rectangle") {
-        rect(40 + 60 * i, 40 + 50 * l, random(30, 40), random(20, 40));
+        rect(50 + 60 * i, 40 + 50 * l, random(30, 40), random(20, 40));
+      }
+      if(randomShape =="half-ellipse-left"){
+        let startAngle = HALF_PI;
+        let endAngle = -HALF_PI;
+        arc(50+60*i, 40 + 50 * l, 30, 30, startAngle, endAngle, PIE);
       }
     }
   }
+  fill(0);
+  textSize(18);
+  text("Bauhaus", 150, 450);
   noLoop();
 }
 
